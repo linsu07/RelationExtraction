8eb53342a3f16720dd01a7492e9740872840932d
linsu07 <lsishere2002@hotmail.com>
2018-10-29 16:56:57 +0800

    readme

--- rlex_net/train.py
+++ rlex_net/train.py
@@ -9,44 +9,40 @@ from rlex_net.parameters import user_params, enrich_hyper_parameters
 from rlex_net.input import SparkInput
 
 flag = tf.flags
-flag.DEFINE_list("ner_procedure",["pcnn","mi_att"], ""
+flag.DEFINE_list("ner_procedure",["pcnn","mi_att"], "layer should be include, order is important"
                                                     "ner处理过程使用的算法，按顺序搭建网络"
                                                     "正确的包括 pcnn,mi_att;"
                                                     "birnn,mi_att"
                                                     "transformer,mi_att2")
 # tf.flags.DEFINE_string("label_voc_file_path", None, "tfrecord中的标签词的字典文件地址，为了兼容spark，目录下唯一text为file")
-tf.flags.DEFINE_string("label_name", "label", "tfrecord中的标签的名字")
+tf.flags.DEFINE_string("label_name", "label", "label's name in tfrecord input files,tfrecord中的标签的名字")
 tf.flags.DEFINE_float("learning_rate", 0.01, '学习率.')
-tf.flags.DEFINE_string("pos_name", "pos", "tfrecord中的position向量列的名字")
+tf.flags.DEFINE_string("pos_name", "pos", "name of word's postition info in tfrecord input files when using pcnn, tfrecord中的position向量列的名字")
 tf.flags.DEFINE_integer("embed_size",100,"如果使用预先训练的embedding，此参数无效，即embedding_file_path 不为None")
-tf.flags.DEFINE_string("embedding_file_path",None,"可选，预训练的embedding文件路径，包括embedding和vocabulary 2个文件，如果不为none，embed_size，feature_voc_file_path参数不起作用")
-tf.flags.DEFINE_string("feature_name", "features", "tfrecord中的特征的名字")
+tf.flags.DEFINE_string("embedding_file_path",None,"optional, pre-trainning wordembedding path ,可选，预训练的embedding文件路径，包括embedding和vocabulary 2个文件，如果不为none，embed_size，feature_voc_file_path参数不起作用")
+tf.flags.DEFINE_string("feature_name", "features", "feature's name in tfrecord input files,tfrecord中的特征的名字")
 # tf.flags.DEFINE_string("feature_voc_file_path", None, "tfrecord中的特征词的字典文件地址，为了兼容spark，目录下唯一text为file")
-tf.flags.DEFINE_integer("filters",64,"cnn卷积核的个数")
-tf.flags.DEFINE_string("cnn_kernel_size","[2,3,5]","cnn 卷积核的长度列表，类似于ngram")
+tf.flags.DEFINE_integer("filters",64,"cnn kerenl numbers, cnn卷积核的个数")
+tf.flags.DEFINE_string("cnn_kernel_size","[2,3,5]","cnn kernel length, cnn 卷积核的长度列表，类似于ngram")
 tf.flags.DEFINE_string('log_level', 'INFO', 'tensorflow训练时的日志打印级别， 取值分别为，DEBUG，INFO,WARN,ERROR')
-tf.flags.DEFINE_string('data_dir', 'd:\\cnn\\tfrecord\\', '训练数据存放路径，支持hdfs')
-tf.flags.DEFINE_string('model_dir', 'd:\\cnn\\model\\', '保存dnn模型文件的路径，支持hdfs')
+tf.flags.DEFINE_string('data_dir', 'd:\\cnn\\tfrecord\\', 'path of trainning,eval data,训练数据存放路径，支持hdfs')
+tf.flags.DEFINE_string('model_dir', 'd:\\cnn\\model\\', 'path for saving model and checkpoint files, 保存dnn模型文件的路径，支持hdfs')
 tf.flags.DEFINE_integer('batch_size', 64, '一批数量样本的数量')
-tf.flags.DEFINE_list("gpu_cores",None,"例如[0,1,2,3]，在当个GPU机器的情况，使用的哪些核来训练")
-tf.flags.DEFINE_integer("check_steps", 300,'保存训练中间结果的间隔，也是evalutation的间隔')
-tf.flags.DEFINE_integer('max_steps', 1000, '训练模型最大的批训练次数，在model_dir不变的情况下重复训练'
+tf.flags.DEFINE_list("gpu_cores",None,"multi-gpu config, gpu ids list,例如[0,1,2,3]，在当个GPU机器的情况，使用的哪些核来训练")
+tf.flags.DEFINE_integer("check_steps", 300,'after how many steps, stopped saving check point and eval, 保存训练中间结果的间隔，也是evalutation的间隔')
+tf.flags.DEFINE_integer('max_steps', 1000, 'after how many steps, stopped and saving model,训练模型最大的批训练次数，在model_dir不变的情况下重复训练'
                                                '，达到max_step后，不再继续训练，或者增加max_step，或者更换model_dir, 再继续训练')
 
-tf.flags.DEFINE_integer("rnn_hidden_size",100,"当使用lstm 时候有效， lstm内部隐藏层大小")
-tf.flags.DEFINE_string("distance_name","distance_name","只在rnn的时候存在，feature的一部分，和语料tensor的结构相同，每个词的内容被一个2个元素的位置代替")
+tf.flags.DEFINE_integer("rnn_hidden_size",100,"birnn's hidden size when using birnn model, 当使用lstm 时候有效， lstm内部隐藏层大小")
+tf.flags.DEFINE_string("distance_name","distance_name","when using birnn, distance from entity A and B are counted, the name of this feature in tfrecord,只在rnn的时候存在，feature的一部分，和语料tensor的结构相同，每个词的内容被一个2个元素的位置代替")
 tf.flags.DEFINE_float("drop_out_rate", 0.5, "dropout概率，范围是0至1。例如rate=0.1会将输入Tensor的内容dropout10%。")
 
 
-tf.flags.DEFINE_integer("enable_ema",0,"是否启动指数移动平均来计算参数")
+tf.flags.DEFINE_integer("enable_ema",0,"if exponent moving average is used, 是否启动指数移动平均来计算参数")
 
-tf.flags.DEFINE_integer("transfromer_layers",2,"使用google 的transformer做encoder的层数")
-tf.flags.DEFINE_integer("transfromer_head_number",8,"使用google 的transformer每一层的head的数量")
-tf.flags.DEFINE_integer("tansformer_d_model",320,"使用google 的transformer每一层feature的维度，最好和embedding_size相等")
-tf.flags.DEFINE_integer("tansformer_shareweight",0,"使用google 的transformer每一层是否共享参数")
-tf.flags.DEFINE_integer("num_classes", 0, "有多少个不同的类别")
-tf.flags.DEFINE_integer("num_features", 0, "有多少个不同的特征值")
-tf.flags.DEFINE_integer("is_multi_label", 0, "是否multi_label。值为0时，表示一条数据有且仅有一个关系label。而值为1时，表示一条数据可能对应多种关系label。")
+tf.flags.DEFINE_integer("num_classes", 0, "how many relations are there.有多少个不同的类别")
+tf.flags.DEFINE_integer("num_features", 0, "how word are there, 有多少个不同的特征值")
+tf.flags.DEFINE_integer("is_multi_label", 0, "is a mutli_relation ? 是否multi_label。值为0时，表示一条数据有且仅有一个关系label。而值为1时，表示一条数据可能对应多种关系label。")
 
 FLAGS = flag.FLAGS
 
